03748ce373c28ae3d17bdd7f5f225b837580a810
Pon20995 <ozera123456789@gmail.com>
2024-02-05 19:13:45 +0000

    added_files_from_4th_lesson

---
+++ lesson4/dir2/debug.js
@@ -0,0 +1,11 @@
+import { n } from "../../nfile.js";
+import { myValue } from "../nfile1.js";
+
+
+console.log(n);
+console.log(myValue);
+
+// // commonjs import->export
+// const myValu2 = require('../nfile1.js')
+// console. log (myValu2)
+
---
+++ lesson4/nfile1.js
@@ -0,0 +1,54 @@
+import chalk from 'chalk';
+export const myValue = 'Text text'
+
+let myNumber2 = 2
+export function add(a, b) {
+    let myNumber = 10
+    myNumber2 = 20
+    return a + b
+}
+
+let result = add(2, 2)
+
+console.log(result)
+console.log(chalk.blue(myNumber2))
+console.log(myNumber2)
+// console.log(myNumber)
+console.log(myValue[0])
+console.log(myValue.length)
+console.log(myValue.toUpperCase())
+console.log(myValue.toLowerCase())
+console.log(myValue.substring(5, 9))
+
+
+
+// // commonjs import->export
+// const myValu2 = 'Text2'
+// module.exports = {myValu2}
+
+
+let result0fDividing = 5 % 2; // 1
+
+// ++
+let counter = 1;
+
+let numberOne = counter++
+console.log(counter)
+// numberOne++;
+// ++numberOne;
+//
+console.log(numberOne)
+
+// a > b; a < b; counter == counterText; counter === counterText;
+// console. log (counter !== counterText)
+// console. log (undefined === null)
+
+
+let javascriptName = "Javascript"
+let newVariable = javascriptName + " " + myValue
+console.log(newVariable)
+
+let newVariableConcat = javascriptName.concat(" ", myValue)
+console.log(newVariableConcat)
+
+let javascriptNameSpaced = " Javascript   "// trim() => "Javascript"
