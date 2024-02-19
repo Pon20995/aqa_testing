2de95da8f3cd6249d38aeb9ddafa81ab8cf32ec2
Pon20995 <ozera123456789@gmail.com>
2024-02-19 19:21:59 +0000

    added_files_from_8th_lesson

---
+++ lesson8/set.js
@@ -0,0 +1,11 @@
+//Set;
+const setNumberOne = new Set([1, 1, 1, 1, 2, 2, 3, 4, 4, 5]);
+const emptySet = new Set();
+console.log(setNumberOne);
+
+setNumberOne.add(6);
+console.log(setNumberOne);
+
+setNumberOne.delete(3);
+console.log(setNumberOne);
+console.log(setNumberOne.size);
