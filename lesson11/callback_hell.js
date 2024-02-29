d854b2f5df4f5e2908cdd1596da75ce97e385147
Pon20995 <ozera123456789@gmail.com>
2024-02-29 19:23:27 +0000

    added_files_from_11th_lesson

---
+++ lesson11/callback_hell.js
@@ -0,0 +1,42 @@
+const menu = {
+  americano: [
+    { item: "🍔", time: 3000 },
+    { item: "🍟", time: 1500 },
+    { item: "🥤", time: 500 },
+  ],
+  italiano: [
+    { item: "🍕", time: 1500 },
+    { item: "🥗", time: 1500 },
+    { item: "🍷", time: 500 },
+  ],
+  breakfast: [
+    { item: "🥪", time: 1000 },
+    { item: "🍳", time: 2500 },
+    { item: "☕", time: 1000 },
+  ],
+};
+const order = (dishes, onComplete) => {
+  console.log(`Start cooking order ...`);
+  const orderResult = [];
+
+  const getResultData = (dish, index) => {
+    orderResult[index] = dish;
+    if (orderResult.filter(Boolean).length === dishes.length)
+      onComplete(orderResult);
+  };
+
+  const cookFood = ({ name: item, time }, index, onDishComplete) => {
+    // const condition = +Math.random().toFixed();
+    const condition = 1;
+
+    setTimeout(() => {
+      const dish = condition
+        ? { status: "fulfilled", value: item }
+        : { status: "rejected", reason: `${item} failed` };
+      onDishComplete(dish, index);
+    }, time);
+  };
+
+  dishes.forEach((item, index) => cookFood(item, index, getResultData));
+};
+order(menu.americano, console.log);
